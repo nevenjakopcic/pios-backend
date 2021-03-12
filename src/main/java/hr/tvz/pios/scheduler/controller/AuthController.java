d74b642e5e7d50dfd9529f8586b5703f36249ca6
Neven Jakopčić <neven.jakopcic@biss.hr>
2021-03-12 23:01:11 +0100

    Remove unnecessary imports

--- src/main/java/hr/tvz/pios/scheduler/controller/AuthController.java
+++ src/main/java/hr/tvz/pios/scheduler/controller/AuthController.java
@@ -4,18 +4,14 @@ import hr.tvz.pios.scheduler.config.JwtTokenUtil;
 import hr.tvz.pios.scheduler.dto.ApiResponse;
 import hr.tvz.pios.scheduler.model.JwtRequest;
 import hr.tvz.pios.scheduler.model.JwtResponse;
-import hr.tvz.pios.scheduler.model.User;
 import hr.tvz.pios.scheduler.service.JwtUserDetailsService;
-import hr.tvz.pios.scheduler.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class AuthController {
 
-    private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtTokenUtil jwtTokenUtil;
     private final JwtUserDetailsService userDetailsService;
